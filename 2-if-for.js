ecd96a7d301826d4eb54b0b95956b53b1881c6a9
sajihassa <gging2105@gmail.com>
2019-09-21 12:19:21 +0700

    Add file

---
+++ 2-if-for.js
@@ -0,0 +1,23 @@
+//const a = "a";
+//if (a === "a") {
+//console.log("It's a");
+//} else {
+//console.log("It's not a");
+//}
+
+//for (let i = 0; i < 10; i++) {
+//console.log(i);
+//}
+//console.log(i);
+
+//let i = 0 ;
+//while (i,< 10; i++) {
+//console.log(i);
+//}
+//console.log(i);
+
+//let sum = 0;
+//for (let i = 1; i <= 1000; i++) {
+//sum += i;
+//console.log(sum);
+//}
---
+++ 4-class.js
@@ -0,0 +1,16 @@
+class Calculator {
+  static sum(x, y) {
+    return x + y;
+  }
+  sum2(x, y) {
+    return x + y;
+  }
+  multiply(x, y) {
+    return x * y;
+  }
+}
+const calculatorObject = new Calculator();
+
+console.log(Calculator.sum(1, 2));
+console.log(calculatorObject.sum2(2, 3));
+console.log(calculatorObject.multiply(5, 6));
